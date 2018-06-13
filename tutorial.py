8d5c8cc402d2adaffd598c9fb61b5ab4a4169819
fuhsinyu <fuhsinyuxinyu@gmail.com>
2018-06-13 04:35:13 +0300

    repo sorted

--- tutorial.py
+++
@@ -1,7 +0,0 @@
-from pyspark import SparkConf, SparkContext
-sc = SparkContext()
-# Create RDD
-intRDD = sc.parallelize([3, 1, 2, 5, 5])
-stringRDD = sc.parallelize(["apple", "bananna", "orange", "grape", "melon"])
-# Transform to Python data structure
-print(intRDD.collect())
